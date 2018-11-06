1e2c988220a6806356279ac6688f3b49e4bcba65
unknown <am.ebrahimzadeh@gmail.com>
2018-11-06 19:09:56 +0330

    add existing file

---
+++ www/js/app.js
@@ -0,0 +1,277 @@
+angular.module("manager", ["ngCordova","ionic","ionMdInput","ionic-material","ion-datetime-picker","ionic.rating","utf8-base64","angular-md5","chart.js","pascalprecht.translate","tmh.dynamicLocale","manager.controllers", "manager.services"])
+	.run(function($ionicPlatform,$window,$interval,$timeout,$ionicHistory,$ionicPopup,$state,$rootScope){
+
+		$rootScope.appName = "manager" ;
+		$rootScope.appLogo = "data/images/header/logo.png" ;
+		$rootScope.appVersion = "1" ;
+		$rootScope.headerShrink = false ;
+
+		$rootScope.liveStatus = "pause" ;
+		$ionicPlatform.ready(function(){
+			$rootScope.liveStatus = "run" ;
+		});
+		$ionicPlatform.on("pause",function(){
+			$rootScope.liveStatus = "pause" ;
+		});
+		$ionicPlatform.on("resume",function(){
+			$rootScope.liveStatus = "run" ;
+		});
+
+		$ionicPlatform.ready(function() {
+
+			localforage.config({
+				driver : [localforage.WEBSQL,localforage.INDEXEDDB,localforage.LOCALSTORAGE],
+				name : "manager",
+				storeName : "manager",
+				description : "The offline datastore for manager app"
+			});
+
+			if(window.cordova){
+				$rootScope.exist_cordova = true ;
+			}else{
+				$rootScope.exist_cordova = false ;
+			}
+			//required: cordova plugin add ionic-plugin-keyboard --save
+			if(window.cordova && window.cordova.plugins.Keyboard) {
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+				cordova.plugins.Keyboard.disableScroll(true);
+			}
+
+			//required: cordova plugin add cordova-plugin-statusbar --save
+			if(window.StatusBar) {
+				StatusBar.styleDefault();
+			}
+
+
+		});
+		$ionicPlatform.registerBackButtonAction(function (e){
+			if($ionicHistory.backView()){
+				$ionicHistory.goBack();
+			}else{
+				$state.go("manager.dashboard");
+			}
+			e.preventDefault();
+			return false;
+		},101);
+	})
+
+
+	.filter("to_trusted", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsHtml(text);
+		};
+	}])
+
+	.filter("trustUrl", function($sce) {
+		return function(url) {
+			return $sce.trustAsResourceUrl(url);
+		};
+	})
+
+	.filter("trustJs", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsJs(text);
+		};
+	}])
+
+	.filter("strExplode", function() {
+		return function($string,$delimiter) {
+			if(!$string.length ) return;
+			var $_delimiter = $delimiter || "|";
+			return $string.split($_delimiter);
+		};
+	})
+
+	.filter("strDate", function(){
+		return function (input) {
+			return new Date(input);
+		}
+	})
+	.filter("phpTime", function(){
+		return function (input) {
+			var timeStamp = parseInt(input) * 1000;
+			return timeStamp ;
+		}
+	})
+	.filter("strHTML", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsHtml(text);
+		};
+	}])
+	.filter("strEscape",function(){
+		return window.encodeURIComponent;
+	})
+	.filter("strUnscape", ["$sce", function($sce) {
+		var div = document.createElement("div");
+		return function(text) {
+			div.innerHTML = text;
+			return $sce.trustAsHtml(div.textContent);
+		};
+	}])
+
+	.filter("stripTags", ["$sce", function($sce){
+		return function(text) {
+			return text.replace(/(<([^>]+)>)/ig,"");
+		};
+	}])
+
+	.filter("chartData", function(){
+		return function (obj) {
+			var new_items = [];
+			angular.forEach(obj, function(child) {
+				var new_item = [];
+				var indeks = 0;
+				angular.forEach(child, function(v){
+						if ((indeks !== 0) && (indeks !== 1)){
+							new_item.push(v);
+						}
+						indeks++;
+					});
+					new_items.push(new_item);
+				});
+			return new_items;
+		}
+	})
+
+	.filter("chartLabels", function(){
+		return function (obj){
+			var new_item = [];
+			angular.forEach(obj, function(child) {
+			var indeks = 0;
+			new_item = [];
+			angular.forEach(child, function(v,l) {
+				if ((indeks !== 0) && (indeks !== 1)) {
+					new_item.push(l);
+				}
+				indeks++;
+			});
+			});
+			return new_item;
+		}
+	})
+	.filter("chartSeries", function(){
+		return function (obj) {
+			var new_items = [];
+			angular.forEach(obj, function(child) {
+				var new_item = [];
+				var indeks = 0;
+				angular.forEach(child, function(v){
+						if (indeks === 1){
+							new_item.push(v);
+						}
+						indeks++;
+					});
+					new_items.push(new_item);
+				});
+			return new_items;
+		}
+	})
+
+
+
+.config(["$translateProvider", function ($translateProvider){
+	$translateProvider.preferredLanguage("fa-ir");
+	$translateProvider.useStaticFilesLoader({
+		prefix: "translations/",
+		suffix: ".json"
+	});
+	$translateProvider.useSanitizeValueStrategy("escapeParameters");
+}])
+
+
+.config(function(tmhDynamicLocaleProvider){
+	tmhDynamicLocaleProvider.localeLocationPattern("lib/ionic/js/i18n/angular-locale_{{locale}}.js");
+	tmhDynamicLocaleProvider.defaultLocale("fa-ir");
+})
+
+
+.config(function($stateProvider, $urlRouterProvider,$sceDelegateProvider,$httpProvider,$ionicConfigProvider){
+	try{
+		// Domain Whitelist
+		$sceDelegateProvider.resourceUrlWhitelist([
+			"self",
+			new RegExp('^(http[s]?):\/\/(w{3}.)?youtube\.com/.+$'),
+			new RegExp('^(http[s]?):\/\/(w{3}.)?w3schools\.com/.+$'),
+		]);
+	}catch(err){
+		console.log("%cerror: %cdomain whitelist","color:blue;font-size:16px;","color:red;font-size:16px;");
+	}
+	$stateProvider
+	.state("manager",{
+		url: "/manager",
+			abstract: true,
+			templateUrl: "templates/manager-side_menus.html",
+			controller: "side_menusCtrl",
+	})
+
+	.state("manager.about_us", {
+		url: "/about_us",
+		views: {
+			"manager-side_menus" : {
+						templateUrl:"templates/manager-about_us.html",
+						controller: "about_usCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+	.state("manager.dashboard", {
+		url: "/dashboard",
+		views: {
+			"manager-side_menus" : {
+						templateUrl:"templates/manager-dashboard.html",
+						controller: "dashboardCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+	.state("manager.menu_one", {
+		url: "/menu_one",
+		views: {
+			"manager-side_menus" : {
+						templateUrl:"templates/manager-menu_one.html",
+						controller: "menu_oneCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+	.state("manager.menu_two", {
+		url: "/menu_two",
+		views: {
+			"manager-side_menus" : {
+						templateUrl:"templates/manager-menu_two.html",
+						controller: "menu_twoCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+	.state("manager.slide_tab_menu", {
+		url: "/slide_tab_menu",
+		views: {
+			"manager-side_menus" : {
+						templateUrl:"templates/manager-slide_tab_menu.html",
+						controller: "slide_tab_menuCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+
+// router by user
+
+
+	$urlRouterProvider.otherwise("/manager/dashboard");
+});
